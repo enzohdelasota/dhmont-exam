eb0b6e7b716163f59074a9db853942f95c92be5a
Lenz Crowell <lhuaricapcha@gmail.com>
2021-03-31 09:29:17 -0500

    add Alert and autofocus

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -8,18 +8,30 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('focus') focus?: ElementRef;
+
   email: string = '';
   password: string = '';
+  failure = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.focus?.nativeElement.focus();
+  }
+
   login() {
     let success = this.authService.login(this.email, this.password);
     if (success) {
       this.router.navigate(['']);
+    } else {
+      this.failure = true;
+      this.email = '';
+      this.password = '';
+      this.focus?.nativeElement.focus();
     }
   }
 
